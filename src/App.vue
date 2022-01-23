ccb17122345cbf61e6fa570bb298dbb32e65d3c0
Benjamin Hollway <benjamin@nothingrandom.com>
2022-01-23 15:54:10 +0000

    feat: import Lato font

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,14 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+
 body {
   background-color: #FFFDEC;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Lato', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
