a805aeef3c022698841fdddd3523d64868376572
Benjamin Hollway <benjamin@nothingrandom.com>
2022-01-23 14:50:24 +0000

    feat: basic random generator

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <Prompt />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Prompt from './components/Prompt.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Prompt
   }
 }
 </script>
---
+++ src/components/Prompt.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="prompt">
+    <h1>{{ this.randomEntry.word.toLowerCase() }}</h1>
+    <h3>{{ this.randomEntry.definition }}</h3>
+    <button @click="this.getRandomEntry()">Generate new</button>
+  </div>
+</template>
+
+<script>
+import dictionary from '../dictionary.json';
+
+export default {
+  name: 'Prompt',
+  data() {
+    return {
+      dictionary: {},
+      dictionaryLength: 0,
+      randomEntry: {
+        word: '',
+        definition: '',
+      },
+    }
+  },
+  mounted() {
+    this.dictionary = dictionary;
+    this.dictionaryLength = Object.keys(this.dictionary).length;
+    this.getRandomEntry();
+  },
+  methods: {
+    randomIntRange(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    getRandomEntry() {
+      const randomNumber = this.randomIntRange(0, this.dictionaryLength);
+      this.randomEntry = {
+        word: Object.keys(this.dictionary)[randomNumber],
+        definition: Object.values(this.dictionary)[randomNumber],
+      }
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
